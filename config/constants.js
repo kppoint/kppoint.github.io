1e7246324d6acb59ad77ab4a5d08ebc377cd2d24
MrOrz <johnsonliang7@gmail.com>
2014-11-18 21:37:51 +0800

    Comments

--- config/constants.js
+++ config/constants.js
@@ -1,5 +1,9 @@
 module.exports = {
   API_ENDPOINT : 'http://kp-point.herokuapp.com/sum',
+
+  // Manual setup of the two numbers.
+  // Used during development and when API server fails during deployment
+  //
   TOTAL_POINTS: 2312,
   TOTAL_USERS: 873
 };
