b4ed6cb3fcd3222b508d7905721d48b313b4cac2
MrOrz <johnsonliang7@gmail.com>
2014-11-18 21:43:47 +0800

    Makes an ajax request on page load.

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
-var $ = require('expose?jQuery!../../vendor/bower_components/jquery/dist/jquery');
+var $ = require('expose?jQuery!../../vendor/bower_components/jquery/dist/jquery'),
+    constants = require('../../config/constants');
+
 require('../../vendor/javascripts/bootstrap');
 
-console.log('Main JS file loaded:', $);
+$.getJSON(constants.API_ENDPOINT, {}, function(data){
+  $('.js-total-points').text(data.total_points);
+  $('.js-total-users').text(data.total_users);
+});
