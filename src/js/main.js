13e78e68af14345761f8d7bbfe0b64c53e7cf7e8
MrOrz <johnsonliang7@gmail.com>
2014-11-20 17:44:13 +0800

    Sends a google analytics event when video is played

--- src/js/main.js
+++ src/js/main.js
@@ -7,3 +7,18 @@ $.getJSON(constants.API_ENDPOINT, {}, function(data){
   $('.js-total-points').text(data.total_points);
   $('.js-total-users').text(data.total_users);
 });
+
+// Loads Youtube video
+window.onYouTubeIframeAPIReady = function(){
+  var player = new window.YT.Player('player', {
+    width: '1280', height: '720',
+    videoId: 'W2b3L-AbVi8', events: {
+      onStateChange: function(evt){
+        // Send google analytics event when video is played.
+        if(evt.data === window.YT.PlayerState.PLAYING){
+          window.ga('send', 'event', 'video', 'play', 'KP advertisement');
+        }
+      }
+    }
+  });
+};
