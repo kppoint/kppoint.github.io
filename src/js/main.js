607752f4960dd4dbd30ce6b9e331a01a25506024
MrOrz <johnsonliang7@gmail.com>
2014-11-21 15:24:29 +0800

    Small adjustments including ESC key detection, desktop scrolling optimization, etc

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ require('../../vendor/javascripts/bootstrap');
 FastClick.attach(document.body);
 
 var $body = $('body'),
-    $window = $('window');
+    $window = $(window);
 
 // Loads data from API server
 //
@@ -19,31 +19,37 @@ $.getJSON(constants.API_ENDPOINT, {}, function(data){
 //
 var $deactivatingElements = $('main, .main-nav a'),
     NAV_OPEN_CLS = 'is-nav-opened';
+
 $('#toggle').click(function(){
   $body.toggleClass(NAV_OPEN_CLS);
 
-  $deactivatingElements.one('click', function(){
-    $body.removeClass(NAV_OPEN_CLS);
-  });
+  onESC(deactivateMainNav)
+  $deactivatingElements.one('click', deactivateMainNav);
 });
 
+
 // Map activation
-var $mapFold = $('.map-fold'), MAP_ACTIVE_CLS = 'is-map-activated';
+//
+var $mapFold = $('.map-fold'),
+    MAP_ACTIVE_CLS = 'is-map-activated';
+
 $mapFold.find('.cover').click(function(){
   window.location.hash = '';
   window.location.hash = '#map';
   $mapFold.addClass(MAP_ACTIVE_CLS);
 
+  onESC(deactivateMap);
+
   window.ga('send', 'event', 'map', 'activate', 'booth');
 });
-$mapFold.find('.deactivate-map').click(function(){ $mapFold.removeClass(MAP_ACTIVE_CLS); });
+$mapFold.find('.deactivate-map').click(deactivateMap);
 
 // Loads Youtube video
 //
 window.onYouTubeIframeAPIReady = function(){
 
   // Wait until all assets loaded
-  $(window).load(function(){
+  $window.load(function(){
     console.log('onLoad called');
     var player = new window.YT.Player('player', {
       width: '1280', height: '720',
@@ -76,11 +82,33 @@ $body.on('click', '[ga-label]', function(){
 
 // Delay the loading of maps after body loads
 //
-$(window).load(function(){
+$window.load(function(){
   console.log('loading maps')
   $('.map-fold iframe').load(function(){
     $('.map-fold .loading').fadeOut('slow');
   }).attr('src', 'https://mapsengine.google.com/map/u/0/embed?mid=zI3lo4kPc9e4.kuZC0pgWAtNs');
+});
+
+
+// Disable pointer events on scroll
+//
+// From: https://developer.mozilla.org/en-US/docs/Web/Events/wheel
+var wheelEvent = "onwheel" in document.createElement("div") ? "wheel" : // Modern browsers support "wheel"
+                document.onmousewheel !== undefined ? "mousewheel" : // Webkit and IE support at least "mousewheel"
+                "DOMMouseScroll"; // let's assume that remaining browsers are older Firefox
+var doneScrollingTimer = null, SCROLL_TIMEOUT = 50;
+
+$window.on(wheelEvent, function(){
+  if(!doneScrollingTimer){
+    $body.css({'pointer-events': 'none'});
+  }
+
+  clearTimeout(doneScrollingTimer);
+
+  doneScrollingTimer = setTimeout(function(){
+    doneScrollingTimer = null;
+    $body.css({'pointer-events': ''});
+  }, SCROLL_TIMEOUT);
 });
 
 // Utility functions
@@ -106,3 +134,21 @@ function animateText($target, to) {
     complete: function(){$target.text(to);}
   });
 }
+
+
+function onESC(callback) {
+  $body.on('keydown.kpesc', function(e){
+    if(e.which === 27) {
+      $body.off('.kpesc');
+      callback();
+    }
+  });
+}
+
+function deactivateMainNav(){
+  $body.removeClass(NAV_OPEN_CLS);
+};
+
+function deactivateMap(){
+  $mapFold.removeClass(MAP_ACTIVE_CLS);
+};
